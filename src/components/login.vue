0a51c6089ae897bd42612345d4d37398f4533f56
苍海之南 <1782920040@qq.com>
2020-03-16 19:46:31 +0800

    Add：引入字体图标

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,28 +1,38 @@
 <template>
   <div class="login_container">
     <div class="login_box">
+      <el-form label-width="80px">
+        <el-form-item label="用户名">
+          <el-input></el-input>
+        </el-form-item>
 
+        <el-form-item label="密码">
+          <el-input></el-input>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+  export default {}
 </script>
 
 <style lang="less" scoped>
-  .login_container{
-    background-color: #2b4b6b;
+  .login_container {
+    background-image: url("../assets/bg.png");
     height: 100%;
   }
 
-  .login_box{
+  .login_box {
     position: absolute;
     left: 50%;
     top: 50%;
-    width: 450px;
-    height: 300px;
-    background-color: white;
+    transform: translate(-50%, -50%);
+    width: 600px;
+    height: 420px;
+    background-color: #fff;
+    box-shadow: 15px 15px 30px rgba(0, 0, 0, .8);
   }
 
 </style>
