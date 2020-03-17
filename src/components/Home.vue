2a3124cc4919b17a9d1d13934fc196bebf547f64
苍海之南 <1782920040@qq.com>
2020-03-17 16:50:27 +0800

    'Add：完善菜单栏'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,18 +11,21 @@
     <!--页面主题-->
     <el-container>
       <!--侧边栏-->
-      <el-aside width="200px">
+      <el-aside :width="isCollapse ? '64px' : '200px'">
         <el-menu
           background-color="#20222A"
           text-color="#fff"
-          active-text-color="#ffd04b">
-
+          active-text-color="#ffd04b" unique-opened :collapse="isCollapse" :collapse-transition="false">
+          <!--折叠展开按钮-->
+          <div class="toggle-button" @click="toggleCollapse">
+            <i class="el-icon-menu"></i>
+          </div>
           <!--一级菜单-->
           <el-submenu :index="item.id + ''" :key="item.id" v-for="item in menus">
             <!--一级菜单模板区-->
             <template slot="title">
               <!--图标-->
-              <i class="el-icon-location"></i>
+              <i :class="iconsObj[item.id]"></i>
               <!--文本-->
               <span>{{item.authName}}</span>
             </template>
@@ -31,7 +34,7 @@
             <el-menu-item :index="subItem.id + ''" :key="subItem.id" v-for="subItem in item.children">
               <template slot="title">
                 <!--图标-->
-                <i class="el-icon-location"></i>
+                <i class="el-icon-menu"></i>
                 <!--文本-->
                 <span>{{subItem.authName}}</span>
               </template>
@@ -51,6 +54,13 @@
       return {
         // 左侧菜单数据
         menus: [],
+        iconsObj: {
+          125: 'iconfont icon-user',
+          103: 'iconfont icon-tijikongjian',
+          101: 'iconfont icon-shangpin',
+          102: 'iconfont icon-danju',
+          145: 'iconfont icon-baobiao'
+        },
         // 是否折叠
         isCollapse: false,
         // 被激活的链接地址
@@ -74,6 +84,10 @@
           return this.$message.error(res.meta.msg)
         }
         this.menus = res.data
+      },
+      // 点击按钮，切换菜单的折叠与展开
+      toggleCollapse () {
+        this.isCollapse = !this.isCollapse
       }
     }
   }
@@ -84,7 +98,7 @@
     display: flex;
     justify-content: space-between;
     background-color: #20222A;
-    color: #B7987B;
+    color: #ffd04b;
     font-size: 20px;
     padding-left: 20px;
     align-items: center;
@@ -97,6 +111,10 @@
 
   .el-aside {
     background-color: #20222A;
+
+    .el-menu {
+      border-right: none;
+    }
   }
 
   .el-main {
@@ -107,4 +125,18 @@
     height: 100%;
   }
 
+  .iconfont {
+    margin-right: 10px;
+  }
+
+  .toggle-button {
+    background-color: #20222A;
+    font-size: 15px;
+    line-height: 24px;
+    margin-left: -4px;
+    color: #fff;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+  }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
+import Login from './components/Login.vue'
+import Home from './components/Home.vue'
 
 Vue.use(VueRouter)
 
